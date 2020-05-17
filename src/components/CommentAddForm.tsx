d12b79c182813adc05bd35420022c156ee9856e5
taishikato <taishi.k0903@gmail.com>
2020-05-16 19:05:02 -0700

    :zap: validation check etc

--- src/components/CommentAddForm.tsx
+++ src/components/CommentAddForm.tsx
@@ -10,6 +10,8 @@ import moment from 'moment-timezone';
 const CommentAddForm: React.FC<IProps> = ({ show, todo }) => {
   const db = useContext(FirestoreContext);
   const [comment, setComment] = useState('');
+  const [isPosting, setisPosting] = useState(false);
+  const [hasError, setHasError] = useState(false);
   const [commentList, setCommentList] = useState<ICommentList[]>([]);
   const loginUser = useSelector<IInitialState, IInitialState['loginUser']>(state => state.loginUser);
   React.useEffect(() => {
@@ -42,25 +44,39 @@ const CommentAddForm: React.FC<IProps> = ({ show, todo }) => {
     }
   }, [show, db, todo, loginUser]);
 
-  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => setComment(e.target.value);
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setHasError(false);
+    setComment(e.target.value);
+  };
   const submit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (comment === '') {
+      setHasError(true);
+      return;
+    }
+    setisPosting(true);
     const id = generateUuid();
+    const commentObj = { id, todoId: todo.id, userId: loginUser.id, text: comment, created: getUnixTime() };
     await db
       .collection('comments')
       .doc(id)
       .set({ id, todoId: todo.id, userId: loginUser.id, text: comment, created: getUnixTime() });
+    if (show) {
+      setCommentList(prev => [...prev, { comment: commentObj, user: loginUser }]);
+    }
+    setComment('');
+    setisPosting(false);
   };
   return (
     <div>
       {commentList.map((commentData: ICommentList) => (
-        <div className="flex mb-3">
+        <div key={commentData.comment.id} className="flex mt-2 mb-2">
           <img src={commentData.user.picture} className="w-6 h-6 rounded-full" alt="" />
           <div key={commentData.comment.id} className="bg-gray-100 rounded ml-2 p-2">
             <div className="font-bold text-sm">{commentData.user.displayName}</div>
             <div className="font-light text-sm">{commentData.comment.text}</div>
             <div className="text-xs text-gray-500 mt-1">
-              {moment.unix(commentData.comment.created).tz('Asia/Tokyo').format('YYYY/MM/DD')}
+              {moment.unix(commentData.comment.created).tz('Asia/Tokyo').format('YYYY/MM/DD H:mm')}
             </div>
           </div>
         </div>
@@ -72,15 +88,23 @@ const CommentAddForm: React.FC<IProps> = ({ show, todo }) => {
             type="text"
             placeholder="コメントを書く"
             className="w-full rounded border-2 border-gray-200 bg-gray-100 p-1 text-sm focus:outline-none"
+            value={comment}
           />
         </div>
-        <div className="px-2 py-1 w-2/12">
-          <input
-            type="submit"
-            value="投稿"
-            className="rounded bg-gray-100 p-1 text-sm border-2 border-gray-100 cursor-pointer hover:bg-gray-200 focus:outline-none"
-          />
+        <div className="px-2 w-2/12">
+          {isPosting ? (
+            <button className="rounded bg-gray-100 px-2 py-1 text-sm border-2 border-gray-100 cursor-not-allowed focus:outline-none">
+              投稿中
+            </button>
+          ) : (
+            <input
+              type="submit"
+              value="投稿"
+              className="rounded bg-gray-200 px-2 py-1 text-sm border-2 border-gray-200 cursor-pointer hover:bg-gray-300 hover:border-gray-300 focus:outline-none"
+            />
+          )}
         </div>
+        {hasError && <p className="text-red-500 text-xs px-2">コメントを入力してください</p>}
       </form>
     </div>
   );
